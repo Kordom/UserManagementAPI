9c1189f2dc2bf945a73a89992d1149ec348b0b23
Kordom <artemij.sakovic@gmail.com>
2026-02-15 20:26:19 +0200

    setup database infrastructure and alembic, simple logging. Fixed issue with credentials being wrong

---
+++ app/core/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+from app.core.config import settings
+
+engine = create_engine(
+    settings.db_url,
+    pool_pre_ping=True,
+)
+
+Session = sessionmaker(
+    autoflush=False,
+    autocommit=False,
+    bind=engine
+)
+
+Base = declarative_base()
