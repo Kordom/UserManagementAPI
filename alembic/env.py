505c9775f4a60b28f9e4aad1ad3520bc6393467b
Kordom <artemij.sakovic@gmail.com>
2026-02-15 22:19:59 +0200

    Setup users table, added password security checks. Added additional imports for correct metadata

--- alembic/env.py
+++ alembic/env.py
@@ -19,6 +19,9 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+from app.core.database import Base
+import app.models
+
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
